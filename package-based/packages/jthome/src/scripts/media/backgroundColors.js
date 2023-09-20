b8a1a92a0ea9adb05d31f76fceae0e53830e5fb9
jtdemey <johntdemey@live.com>
2023-09-19 23:26:40 -0400

    Contact page

--- package-based/packages/jthome/src/scripts/media/backgroundColors.js
+++ package-based/packages/jthome/src/scripts/media/backgroundColors.js
@@ -1,4 +1,4 @@
-//About #595F62, Skills #011936, Contact #273F3A, Doodles #175873
-const BackgroundColors = ['#45050C', '#011936', '#273F3A', '#175873'];
+//Media #595F62, NSG #120316, Meeting Minutes #273F3A, Things to Know #175873
+const BackgroundColors = ["#45050C", "#120316", "#273F3A", "#175873"];
 
 export default BackgroundColors;
--- package-based/packages/jthome/src/scripts/media/loadDoodles.js
+++ package-based/packages/jthome/src/scripts/media/loadDoodles.js
@@ -1,20 +1,20 @@
 const loadDoodle = async (ind, isPreview) => {
   const uri = `${window.location.origin}/img/doodles/`;
   const dood = new Image();
-  dood.src = `${uri}${isPreview ? 'thumbs/' : ''}img${ind}.webp`;
+  dood.src = `${uri}${isPreview ? "thumbs/" : ""}img${ind}.webp`;
 
-  const link = document.createElement('a');
+  const link = document.createElement("a");
   link.href = `${uri}img${ind}.webp`;
-  link.setAttribute('target', '_blank');
-  link.setAttribute('rel', 'noopener noreferrer');
+  link.setAttribute("target", "_blank");
+  link.setAttribute("rel", "noopener noreferrer");
   link.appendChild(dood);
 
-  const card = document.createElement('article');
-  card.classList.add(isPreview ? 'doodle-card' : 'drawing');
+  const card = document.createElement("article");
+  card.classList.add(isPreview ? "doodle-card" : "drawing");
   card.appendChild(link);
 
   const cards = document.querySelector(
-    isPreview ? '.doodle-cards' : '.drawings'
+    isPreview ? ".doodle-cards" : ".drawings"
   );
   cards.appendChild(card);
 };
@@ -26,16 +26,16 @@ export const loadDoodles = (amt, uiState) => {
       uiState.doodlesLoaded += 1;
       loadDoodle(i, false);
     } else {
-      document.querySelector('#load-doodles-btn').style.display = 'none';
+      document.querySelector("#load-doodles-btn").style.display = "none";
     }
   }
 };
 
-export const loadDoodlePreviews = uiState => {
+export const loadDoodlePreviews = (uiState) => {
   const randInts = [];
   while (randInts.length < 5) {
-    const ind = Math.floor(Math.random() * uiState.doodleCt);
-    if (!randInts.some(int => int === ind)) {
+    const ind = Math.floor(Math.random() * uiState.doodleTotal);
+    if (!randInts.some((int) => int === ind)) {
       randInts.push(ind);
     }
   }
