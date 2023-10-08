15f65c10e1850a1df2eb8a03a094f8b0dda8ee29
jtdemey <johntdemey@live.com>
2023-10-08 00:48:54 -0400

    PGP key

--- package-based/packages/jthome/src/scripts/contact/contactIndex.js
+++ package-based/packages/jthome/src/scripts/contact/contactIndex.js
@@ -1,22 +1,6 @@
-import backgroundColors from "./backgroundColors";
-import uiState from "./uiState";
-import { startBgShift } from "../lib/backgroundShifter";
-import { resizeUpdate } from "../lib/resize";
-import { scrollPoll } from "../lib/scroll";
-
-const siteWrapper = document.querySelector(".site-wrapper");
-const mainContainer = document.querySelector(".main-container");
-const mobileNav = document.querySelector(".mnav-area");
-const contentViews = document.querySelectorAll(".content-view");
-
-uiState.mobile = window.getComputedStyle(mobileNav).display === "block";
-const shiftBg = (viewInd) =>
-  startBgShift(backgroundColors, mainContainer, uiState, viewInd);
-setInterval(() => scrollPoll(mobileNav, siteWrapper, shiftBg, uiState), 500);
-const triggerResize = () => resizeUpdate(contentViews, mobileNav, uiState);
-window.addEventListener("resize", triggerResize);
-setTimeout(() => {
-  uiState.contentHeight = contentViews[0].clientHeight;
-  triggerResize();
-}, 500);
-mainContainer.style.transition = "background 1.2s";
+const copyKeyBtn = document.getElementById("copy-pgp-key-btn");
+copyKeyBtn.addEventListener("click", () => {
+  const keyElement = document.querySelector("blockquote > p");
+  const text = keyElement.innerHTML.replace(/<br>/g, "\n");
+  navigator.clipboard.writeText(text);
+});
