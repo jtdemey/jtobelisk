75e635242280c773809cf6c51bba2c03770840b4
jtdemey <johntdemey@live.com>
2024-02-02 22:45:26 -0500

    Poems link

--- package-based/packages/jthome/src/scripts/media/backgroundColors.js
+++ package-based/packages/jthome/src/scripts/media/backgroundColors.js
@@ -1,4 +1,4 @@
-//Media #45050C, NSG #120316, Meeting Minutes #273F3A, Things to Know #175873
-const BackgroundColors = ["#45050C", "#120316", "#273F3A", "#175873"];
+//Media #45050C, NSG #20260d, Meeting Minutes #273F3A, Things to Know #175873
+const BackgroundColors = ["#45050C", "#20260d", "#273F3A", "#175873"];
 
 export default BackgroundColors;
