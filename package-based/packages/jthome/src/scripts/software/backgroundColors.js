82324f5ded2a0fed41e745a727b7318b22d25e86
JD <johntdemey@live.com>
2025-01-26 12:00:46 -0500

    Fix image tags, update contact methods, hire me page

--- package-based/packages/jthome/src/scripts/software/backgroundColors.js
+++ package-based/packages/jthome/src/scripts/software/backgroundColors.js
@@ -1,4 +1,4 @@
-//Software #3f415a, Ncryptor #1d3021, KeyringBot #344e41, TDF #3c413c
-const BackgroundColors = ["#3f415a", "#1d3021", "#344e41", "#3c413c"];
+//Software #3f415a, Ncryptor #1d3021, KeyringBot #344e41, TaleWeaver hsl(60, 13%, 27%) 
+const BackgroundColors = ["#3f415a", "#1d3021", "#344e41", "hsl(60, 13%, 27%)"];
 
 export default BackgroundColors;
